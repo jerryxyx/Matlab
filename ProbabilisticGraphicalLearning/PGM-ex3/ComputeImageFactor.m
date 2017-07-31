4c94c93da5a57554ac02f5cf182f9f506f2ee218
Yuxuan Xia <Yuxuan Xia>
2017-07-31 11:44:32 -0400

    ex3

---
+++ ProbabilisticGraphicalLearning/PGM-ex3/ComputeImageFactor.m
@@ -0,0 +1,33 @@
+function P = ComputeImageFactor (img, imgModel)
+% This function computes the singleton OCR factor values for a single
+% image.
+%
+% Input:
+%   img: The 16x8 matrix of the image
+%   imgModel: The provided, trained image model
+%
+% Output:
+%   P: A K-by-1 array of the factor values for each of the K possible
+%     character assignments to the given image
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+X = img(:);
+N = length(X);
+K = imgModel.K;
+
+theta = reshape(imgModel.params(1:N*(K-1)), K-1, N);
+bias  = reshape(imgModel.params((1+N*(K-1)):end), K-1, 1);
+
+%old version 
+%W = [ bsxfun(@plus, theta * X, bias) ; 0 ];
+%W = bsxfun(@minus, W, max(W));
+%W = exp(W);
+%P=bsxfun(@rdivide, W, sum(W));
+
+W = [theta*X+bias; 0];
+W = W - max(W);
+W = exp(W);
+P = W./sum(W);
+end
+
